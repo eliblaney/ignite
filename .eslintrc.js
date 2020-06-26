70a026accb78382700aae96612b4b6eef7b781d0
Eli Blaney <eliblaney@gmail.com>
2020-06-25 20:02:05 -0500

    Configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,42 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  "plugins": [
+    "react",
+    "react-native",
+    "prettier"
+  ],
+  "parserOptions": {
+      "ecmaFeatures": {
+          "jsx": true
+      }
+  },
+  'extends': [
+    'airbnb',
+    '@react-native-community',
+    "plugin:prettier/recommended"
+  ],
+  'parser': 'babel-eslint',
+  'env': {
+    'jest': true,
+    "react-native/react-native": true
+  },
+  'globals': {
+    "fetch": false
+  },
+  'rules': {
+    "prettier/prettier": 1,
+    "no-param-reassign": 0,
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'comma-dangle': 'off',
+    "react-native/no-unused-styles": 2,
+    "react-native/no-raw-text": 2,
+    "react-native/no-single-element-style-arrays": 2,
+    "keyword-spacing": ["error", { "overrides": {
+      "if": { "after": false },
+      "for": { "after": false },
+      "while": { "after": false }
+    }}],
+  }
 };
