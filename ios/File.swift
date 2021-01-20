06612ff8903d689f7f8013f3eb030cc28a815424
Eli Blaney <eliblaney@gmail.com>
2021-01-19 18:49:15 -0600

    Update to Xcode 12

---
+++ ios/File.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  Ignite
+//
+//  Created by Eli Blaney on 1/19/21.
+//
+
+import Foundation
+
+// This file only exists so that I can get the bridging header to allow Swift libraries to be included in the final build. Thanks Apple.
