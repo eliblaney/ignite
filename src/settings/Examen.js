2c5e399d7dca4cb2b4557059af80a77c7661ac21
Eli Blaney <eliblaney@gmail.com>
2021-02-06 11:34:08 -0600

    Make examen audio load on component mount

--- src/settings/Examen.js
+++ src/settings/Examen.js
@@ -25,6 +25,10 @@ export default withNavigation(
         "1. **Begin by putting yourself in the presence of God. Create a quiet, prayerful space within your mind and heart. Draw near to the flame of God’s love.**\n\n* Breathe in the grace of God that surrounds you. Become aware of God’s presence with you right now.\n\n* Thank God for accompanying you on your spiritual journey, for allowing you to know God more deeply.\n\n* Thank God for being present to you, even when you do not recognize God’s presence.\n\n2. **Ask God to help you as you review the events of your day with gratitude in your mind’s eye through the gift of your memory.**\n\n* You deeply desire the ability to review the events of your day with clarity.\n\n* Invite God to sit down with you and review these events together.\n\n* Begin with the start of the day. (What were you feeling as you woke up? What did you eat for breakfast, if you ate at all? How were you feeling in each class?)\n\n* See the times in your day when you turned to or felt close to God. Perhaps you felt a spark of God’s presence. Through all of this, be aware of your emotions with each event.\n\n* See the times in your day when you turned away from God or from your true self. Where did you turn away or close yourself off from the fire of God? Again, be aware of your emotions with each event.\n\n* What have you noticed about your practice of fasting? Have you experienced any freedom or grace through the fast recently?\n\n3. Choose one feature of the day and pray from it.\n\n* After you review your day, select one experience, event or feeling and contemplate it for a few minutes with God. Talk with God about it.\n\n* Do you sense how God feels about that event?\n\n* What grace, even if it is hard to see initially, did you experience?\n";
     }
 
+    componentDidMount() {
+      this.createTrackPlayer();
+    }
+
     isPlaying = async () => {
       const state = await TrackPlayer.getState();
       return state === "playing";
